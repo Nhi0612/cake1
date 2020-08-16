ff9aa69f54b514be56dc5e9e6754f3dae6eb6e18
Nhi0612 <66456615+Nhi0612@users.noreply.github.com>
2020-08-16 22:57:42 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="./1.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    </head>
+    <body>
+        <header>
+            <div class="container">
+                <nav class="btn-group" role="group" aria-label="Basic example">
+                    <img src="./img/logo.jpg" alt="123" class="logo">
+                    <button type="button" class="btn btn1"> Bánh Ngọt </button>
+                    <button type="button" class="btn btn1"> Bánh Mặn</button>
+                    <button type="button" class="btn btn1"> Nước Uống</button>
+                    <img src="./img/cart.png" alt="cart" class="cart">    
+                </nav>
+            </div>
+            
+        </header>
+        <main>
+            <img src="./img/show1.jpg" alt="show" class="showside">
+            
+                <div class="row ">
+                        <h2 class="text1">Banh Ngot</h2>
+                </div>
+                <div class="container">
+                <div class="row">
+                    <div class="product">
+                        <figure  class="figure" >
+                            <img src="./img/sh1.jpg" alt="sh" class="imgs">
+                            <div class="action">
+                                <button type="button" class="btn2" > Xem</button>
+                            </div>
+                        </figure>
+                        </div>
+                    <div class="product">
+                        <figure  class="figure" >
+                            <img src="./img/sh2.jpg" alt="sh" class="imgs">
+                            <div class="action">
+                                <button type="button" class="btn2" > Xem</button>
+                            </div>
+                        </figure>
+                    </div>
+                    <div class="product">
+                        <figure  class="figure">
+                            <img src="./img/sh3.jpg" alt="sh3" class="imgs">
+                            <div class="action">
+                                <button type="button" class="btn2" > Xem</button>
+                            </div>
+                        </figure>
+                    </div>
+                    <div class="product">
+                        <figure  class="figure">
+                            <img src="./img/sh5.jpg" alt="sh4" class=" imgs">
+                            <div class="action">
+                                <button type="button" class="btn2" > Xem</button>
+                            </div>
+                        </figure>
+                    </div>
+                        
+                    </div>
+                </div>
+                <div class="row">
+                    <h2 class="text1">Banh Man</h2>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="product">
+                            <figure  class="figure" >
+                                <img src="./img/sm1.jpg" alt="sm1" class="imgs">
+                                <div class="action">
+                                    <button type="button" class="btn2" > Xem</button>
+                                </div>
+                            </figure>
+                         </div>
+
+                         <div class="product">
+                            <figure  class="figure" >
+                                <img src="./img/sm3.jpg" alt="sm1" class="imgs">
+                                <div class="action">
+                                    <button type="button" class="btn2" > Xem</button>
+                                </div>
+                            </figure>
+                         </div>
+                         <div class="product">
+                            <figure  class="figure" >
+                                <img src="./img/sm4.jpg" alt="sm1" class="imgs">
+                                <div class="action">
+                                    <button type="button" class="btn2" > Xem</button>
+                                </div>
+                            </figure>
+                         </div>
+                         <div class="product">
+                            <figure  class="figure" >
+                                <img src="./img/sm5.jpg" alt="sm1" class="imgs">
+                                <div class="action">
+                                    <button type="button" class="btn2" > Xem</button>
+                                </div>
+                            </figure>
+                         </div>
+                    </div>  
+                </div>
+               
+                <div class="row">
+                    <h2 class="text1">Nuoc Uong</h2>
+                </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="product">
+                            <figure  class="figure" >
+                                <img src="./img/su1.jpg" alt="sm1" class="imgs">
+                                <div class="action">
+                                    <button type="button" class="btn2" > Xem</button>
+                                </div>
+                            </figure>
+                         </div>
+
+                         <div class="product">
+                            <figure  class="figure" >
+                                <img src="./img/su2.jpg" alt="sm1" class="imgs">
+                                <div class="action">
+                                    <button type="button" class="btn2" > Xem</button>
+                                </div>
+                            </figure>
+                         </div>
+                         <div class="product">
+                            <figure  class="figure" >
+                                <img src="./img/su4.jpg" alt="sm1" class="imgs">
+                                <div class="action">
+                                    <button type="button" class="btn2" > Xem</button>
+                                </div>
+                            </figure>
+                         </div>
+                         <div class="product">
+                            <figure  class="figure" >
+                                <img src="./img/su5.png" alt="sm1" class="imgs">
+                                <div class="action">
+                                    <button type="button" class="btn2" > Xem</button>
+                                </div>
+                            </figure>
+                         </div>
+                    </div>  
+                </div>
+                
+
+        </main>
+        <footer>
+            <footer class="background3 mt-5">
+                <div class="container">
+                  <div class="row">
+                    <div class="col text4">
+                        <p><b>COMPANY INFO</b></p>
+                        <h6>About ATN</h6>
+                    </div>
+                    <div class="col text4">
+                      <p><b>HELP & SUPPORT</b></p>
+                      <h6>Shipping Info</h6>
+                      <h6>Returns</h6>
+                      <h6>How To Order</h6>
+                      <h6>How To Track</h6>
+                      <h6>Size Guide</h6>
+                    </div>
+                    <div class="col text4 ">
+                     <p><b>CUSTOMER CARE</b></p>
+                     <h6>Contact Us</h6>
+                     <h6>Payment Method</h6>
+                     <h6>Bonus Point</h6>
+                    </div>
+                    <div class="col text4">
+                      <p><b>FIND US ON</b></p>
+                      <img src="./img/facebook.png" alt="facebook" class="image6">
+                      <img src="./img/ima.png" alt="ima" class="image6">
+                      <img src="./img/ima2.png" alt="ima2" class="image6">
+                      <img src="./img/ima3.png" alt="ima3" class="image6">
+                      <img src="./img/ima4.png" alt="ima4" class="image6">
+                    </div>
+                  </div>
+                </div>
+              </footer>
+            <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+        </footer>
+
+    </body>
+</html>
